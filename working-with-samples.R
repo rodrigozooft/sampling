92c2a0e48076159c214001c1c066082726ffdf51
Rod Morgado <rodrigo@zooft.company>
2022-02-22 20:19:44 -0800

    using slice_sample(prop = 1) to shuffle the rows. Systematic sampling has problems when the data are sorted or contain a pattern.

--- working-with-samples.R
+++ working-with-samples.R
@@ -60,3 +60,24 @@ attrition_sys_samp <- attrition_pop %>%
 
 # See the result
 View(attrition_sys_samp)
+
+# Add a row ID column to attrition_pop
+attrition_pop_id <- attrition_pop %>% 
+  rowid_to_column()
+
+# Using attrition_pop_id, plot YearsAtCompany vs. rowid
+ggplot(attrition_pop_id, aes(y = YearsAtCompany, x = rowid)) +
+  # Make it a scatter plot
+  geom_point() +
+  # Add a smooth trend line
+  geom_smooth()
+
+# Shuffle the rows of attrition_pop then add row IDs
+attrition_shuffled <- attrition_pop %>% slice_sample(prop = 1) %>% rowid_to_column()
+
+# Using attrition_shuffled, plot YearsAtCompany vs. rowid
+# Add points and a smooth trend line
+ggplot(attrition_shuffled, aes(y = YearsAtCompany, x = rowid)) +
+    geom_point() + 
+    geom_smooth()
+

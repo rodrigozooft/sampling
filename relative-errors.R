6b4c1cd592e0b344791505802ac27eceeee6407f
Rod Morgado <rodrigo@zooft.company>
2022-02-23 20:57:06 -0800

    By generating the sample statistic many times with different samples, you can quantify the amount of variation in those statistics

--- relative-errors.R
+++ relative-errors.R
@@ -7,3 +7,23 @@ rel_error_pct100 <- (abs(mean_attrition_pop - mean_attrition_srs100) / mean_attr
 
 # See the result
 rel_error_pct100
+
+# Replicate this code 500 times
+mean_attritions <- replicate(
+  n = 500, 
+  expr = 
+    attrition_pop %>% 
+      slice_sample(n = 20) %>% 
+      summarize(mean_attrition = mean(Attrition == "Yes")) %>% 
+      pull(mean_attrition)
+)
+
+# See the result
+head(mean_attritions)
+
+# Store mean_attritions in a tibble in a column named sample_mean
+sample_means <- tibble(sample_mean = mean_attritions)
+
+# Plot a histogram of the `sample_mean` column, binwidth 0.05
+ggplot(sample_means, aes(sample_mean)) + 
+  geom_histogram(binwidth = 0.05

abd9675aaeb73dc7d6d35bce0aff61c16a35de21
Rod Morgado <rodrigo@zooft.company>
2022-03-25 21:57:39 -0700

    The test compare proportions of successes in a categorical variable across groups of another categorical variable is called a chi-square test of independence.

--- running-tests.R
+++ running-tests.R
@@ -3,3 +3,9 @@
 ggplot(late_shipments, aes(vendor_inco_term, fill = freight_cost_group)) +
     geom_bar(position = "fill") +
     ylab("proportion")
+
+# Perform a chi-square test of independence on freight_cost_group and vendor_inco_term
+test_results <- late_shipments %>% chisq_test(freight_cost_group ~ vendor_inco_term)
+
+# See the results
+test_results

886a52138738beef95c8a5e9b5b20253a9979215
Rod Morgado <rodrigo@zooft.company>
2022-03-31 17:08:29 -0700

    Notice that tthe statistic for the latter datasse, natarms and party, was for more into he tails of the null distribution than the statistic corresponding to the relationship between natspac and party

--- running-tests.R
+++ running-tests.R
@@ -853,3 +853,32 @@ ggplot(perm_1, aes(x = party, fill = natarms)) +
 # Compute chi-squared stat
 gss_party %>%
   chisq_stat(natarms ~ party)
+
+# Create null
+null_spac <- gss_party %>%
+  specify(natspac ~ party) %>%
+  hypothesize(null = "independence") %>%
+  generate(reps = 500, type = "permute") %>%
+  calculate(stat = "Chisq")
+
+# Visualize null
+ggplot(null_spac, aes(x = stat)) +
+  # Add density layer
+  geom_density() +
+  # Add vertical line at obs stat
+  geom_vline(xintercept = chi_obs_spac, color = "red")
+
+# Create null that natarms and party are indep
+null_arms <- gss_party %>%
+  specify(natarms ~ party) %>%
+  hypothesize(null = "independence") %>%
+  generate(reps = 500, type = "permute") %>%
+  calculate(stat = "Chisq")
+  
+# Visualize null
+ggplot(null_arms, aes(x = stat)) +
+  # Add density layer
+  geom_density() +
+  # Add vertical red line at obs stat
+  geom_vline(xintercept = chi_obs_arms, color = "red")
+

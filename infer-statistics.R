1e17bc3c41e44915c66fc6df62765eb756c20e4e
Rod Morgado <rodrigo@zooft.company>
2022-04-01 21:57:15 -0700

    generating a new bootstrap distribution to see if the median weight of babies in pounds is greater than 7.

--- infer-statistics.R
+++ infer-statistics.R
@@ -77,3 +77,24 @@ rent_med_ht %>%
   filter(stat >= rent_med_obs) %>%
   # Calculate the p-value
   summarize(p_val = n() / 15000)
+
+# From previous steps
+n_replicates <- 1500
+weight_mean_ht <- ncbirths %>%
+  specify(response = weight) %>%
+  hypothesize(null = "point", mu = 7) %>% 
+  generate(reps = n_replicates, type = "bootstrap") %>% 
+  calculate(stat = "mean")
+weight_mean_obs <- ncbirths %>%
+  summarize(mean_weight = mean(weight)) %>%
+  pull()
+
+# Calculate p-value
+weight_mean_ht %>%
+  # Filter on stat greater than or equal to weight_mean_obs
+  filter(stat >= weight_mean_obs) %>%
+  # p_val is twice the number of filtered rows divided by the total number of rows
+  summarize(
+    one_sided_p_val = n() / n_replicates,
+    two_sided_p_val = 2 * one_sided_p_val
+  )

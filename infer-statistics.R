5e1418851a3261b8033b32c81cbd3de53592c519
Rod Morgado <rodrigo@zooft.company>
2022-04-03 11:29:39 -0700

    You have too compare your resulting p-value to the modified significance level

--- infer-statistics.R
+++ infer-statistics.R
@@ -287,3 +287,21 @@ ggplot(gss, aes(wordsum)) +
   geom_histogram(binwidth = 1) +
   # Facet by class
   facet_wrap(~class)
+
+# Run an analysis of variance on wordsum vs. class
+aov_wordsum_class <- aov(wordsum ~ class, data = gss)
+
+# Tidy the model
+tidy(aov_wordsum_class)
+
+gss %>%
+  # Group by class
+  group_by(class) %>%
+  # Calculate the std dev of wordsum as std_dev_wordsum
+  summarize(std_dev_wordsum = sd(wordsum))
+
+# Run a pairwise t-test on wordsum and class, without adjustment
+t_test_results <- pairwise.t.test(gss$wordsum, gss$class, p.adjust.method = "none")
+
+# Tidy the result
+tidy(t_test_results)
